5941fcc1912c885d4e5aa16968a18fa8a8ead45c
Lorenzo Tronchin <lorenzotronchin@gmail.com>
2022-07-22 13:01:37 +0200

    <>

--- src/bash/claro-num_191-modalities_CT-aug_ada-aug_opts_xflip,xint,scale,rotate,aniso,xfrac-val_method_bootstrap-exps_5-fold_0.sh
+++ src/bash/claro-num_191-modalities_CT-aug_ada-aug_opts_xflip,xint,scale,rotate,aniso,xfrac-val_method_bootstrap-exps_5-fold_0.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#SBATCH -A PROJECTID  -p alvis
+#SBATCH -A snic2022-5-277 -p alvis
 #SBATCH -N 1 --gpus-per-node=A100:1
 #SBATCH -t 0-24:00:00
 #SBATCH --error=job_%J.err
 #SBATCH --output=out_%J.out
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=email
+#SBATCH --mail-user=l.tronchin@unicampus.it
 
 module load CUDA/11.3.1
 module load Python/3.8.6-GCCcore-10.2.0
